b86e1b4563647e24995342fc75b3b1337a1247bc
Arjun santhosh <124417632+Arjunsanthosh09@users.noreply.github.com>
2024-12-31 06:57:20 +0530

    Add files via upload

---
+++ Lab record/Simple programs/arraysort.java
@@ -0,0 +1,36 @@
+//Sort n numbers in a array
+
+import java.io.DataInputStream;
+import java.util.Arrays;
+public class arraysort {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        DataInputStream in=new DataInputStream(System.in);
+        int n;
+        try {
+            System.out.println("Enter the size of the array  :");
+            n=Integer.parseInt(in.readLine());
+            int number[]=new int[n];
+            for(int i=0;i<n;i++){
+                System.out.println("Enter the value "+i+1);
+                number[i]=Integer.parseInt(in.readLine());
+            }
+            System.out.println("Array Before sorting..");
+            for(int num : number){
+                System.out.println(num);
+            }
+            System.out.println("Array After sorting..");
+            Arrays.sort(number);
+            for(int sorted : number){
+                System.out.println(sorted);
+            }
+
+        } catch (Exception e) {
+        }
+    
+    }
+
+        
+        
+}
+
